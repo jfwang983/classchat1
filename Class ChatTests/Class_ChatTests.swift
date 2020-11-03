abb26fca6f28ece38a3f936fef7131c520817c80
Benjamin Garcia <48642473+benjamingarcia10@users.noreply.github.com>
2020-11-02 18:34:55 -0800

    Uploaded original project files

---
+++ Class ChatTests/Class_ChatTests.swift
@@ -0,0 +1,33 @@
+//
+//  Class_ChatTests.swift
+//  Class ChatTests
+//
+//  Created by Benjamin Garcia on 11/2/20.
+//
+
+import XCTest
+@testable import Class_Chat
+
+class Class_ChatTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
