f98fc556ea3a7e6f58286be79706c424003a4acd
Benjamin Garcia <48642473+benjamingarcia10@users.noreply.github.com>
2020-11-16 23:49:25 -0800

    Implemented login and logout

--- Class Chat/AppDelegate.swift
+++ Class Chat/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let parseConfig = ParseClientConfiguration {
+            $0.applicationId = "ukyQWjzSsUS83qLYyXfJ5g99F7G15wqZDfI4V2pI"
+            $0.clientKey = "JOSLp8HcpkbTJVxUW5AI39xBXlwdbkfWzGMB5FfT"
+            $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: parseConfig)
+        
         return true
     }
 
---
+++ Class Chat/View Controllers/ClassesViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ClassesViewController.swift
+//  Class Chat
+//
+//  Created by Benjamin Garcia on 11/16/20.
+//
+
+import UIKit
+import Parse
+
+class ClassesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onLogout(_ sender: Any) {
+        PFUser.logOut()
+        
+        let main = UIStoryboard(name: "Main", bundle: nil)
+        let loginViewController = main.instantiateViewController(withIdentifier: "LoginViewController")
+        
+        let scene = UIApplication.shared.connectedScenes.first
+        if let delegate: SceneDelegate = (scene?.delegate as? SceneDelegate) {
+            delegate.window?.rootViewController = loginViewController
+        }
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
