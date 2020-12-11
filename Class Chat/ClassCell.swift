f459fae4ff4c492f846f9ac586f657dbeeb2723d
Angeli Faith Deanon <afdeanon@gmail.com>
2020-12-10 23:37:49 -0800

    connected some IBOutlets

---
+++ Class Chat/ClassCell.swift
@@ -0,0 +1,25 @@
+//
+//  ClassCell.swift
+//  Class Chat
+//
+//  Created by Angeli Faith Deanon on 12/10/20.
+//
+
+import UIKit
+
+class ClassCell: UITableViewCell {
+    @IBOutlet weak var className: UILabel!
+    @IBOutlet weak var teacherName: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Class Chat/View Controllers/ClassesViewController.swift
+++ Class Chat/View Controllers/ClassesViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 import Parse
 
 class ClassesViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
